9a5e283a4331d3c377f58da658e50a25ee5cf3c6
Jeremy Ellis <keyfreemusic@gmail.com>
2022-06-11 18:11:54 -0700

    Update serial.js

--- demos/serial.js
+++ demos/serial.js
@@ -23,6 +23,7 @@ var serial = {};
       { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
       { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
       { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
+      { 'vendorId': 0x2341, 'productId': 0x025B }, // Arduino PortentaH7
       { 'vendorId': 0x239A }, // Adafruit Boards!
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
@@ -30,6 +31,8 @@ var serial = {};
     );
   }
 
+  
+  //pid=025B 
   serial.Port = function(device) {
     this.device_ = device;
     this.interfaceNumber_ = 2;  // original interface number of WebUSB Arduino demo
